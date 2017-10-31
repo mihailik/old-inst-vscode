36c83ad314a1a026572adab9c4ee5ea3a5f7ac3b
Oleg Mihailik <mihailik@gmail.com>
2017-10-31 08:08:29 +0000

    Reorder to expose naked binaries in platform dependencies.

--- vscode-bin-darwin/code.js
+++ vscode-bin-darwin/code.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-var fs = require('fs');
-var path = require('path');
-var child_process = require('child_process');
+if (process.mainModule === module) {
+  console.log('THIS IS NOT A SCRIPT, CANNOT RUN');
+}
 
 module.exports = {
   zip: path.resolve(__dirname, 'VSCode-darwin-stable.zip'),
